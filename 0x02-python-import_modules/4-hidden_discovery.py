e09ddea1c4c1a45a5fbd5742599de8e30bd40970
EseVic <esevic111@gmail.com>
2023-05-11 06:47:40 -0700

    variable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    for j in dir(hidden_4):
-        if not j.startswith('__'):
+    for i in dir(hidden_4):
+        if not i.startswith('__'):
             print(i)
