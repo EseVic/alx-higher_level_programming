13c610da453bdef3e1c6de7693e1e93cf57baa9e
EseVic <esevic111@gmail.com>
2023-05-17 15:45:02 -0700

    multiply

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    diff = set_1 ^ set_2
-    return diff
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x*number, my_list)))
