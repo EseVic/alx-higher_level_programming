f523a67dcefc5a502ed77966f3380a2d5cb59f93
EseVic <esevic111@gmail.com>
2023-06-10 07:27:56 -0700

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,9 @@
 ''' function that returns the list
 '''
 
+
 def lookup(obj):
-''' function: lookup()
-    Returns a list object
+    ''' function: lookup()
+    Returns a list of an object available attribute
     '''
-    return(dir(obj))
+    return dir(obj)
